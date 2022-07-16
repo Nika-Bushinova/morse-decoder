08a5bb9b9d1e52fa6f936799d25d7524cb008718
Nika-Bushinova <n190696@yandex.ru>
2022-07-16 19:18:01 -0400

    add:feature

--- src/index.js
+++ src/index.js
@@ -38,8 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr2 = expr.split('')
+    let arr = []
+    function change(numArr) {
+       let resultF = []
+       let result = []
+       let result3 = []
+       let result4 = []
+       result5 = []
+       let el2
+       numArr.forEach((element) => {
+          resultF.push(element.join(''))
+          resultF.forEach((el, i) => {
+             return el === '10' ? el2 = '.' : el === '11' ? el2 = '-' : el === '**' ? el2 = ' ' : el === '00' ? el2 = '' : el2 = ''
+          })
+          result.push(el2)
+       })
+        while (result.length > 0) {
+          result4.push(result.splice(0, 5).join(''))
+       }
+        
+          for (let i = 0; i < result4.length; i++) {
+             if(result4[i]==='     '){result5.push(' ')}
+          for (let key in MORSE_TABLE) {
+ 
+             if (result4[i] === key) {
+                result5.push(MORSE_TABLE[key])
+             }
+ 
+          }
+       } 
+       return result5.join('')
+    }
+    while (arr2.length > 0) {
+       arr.push(arr2.splice(0, 2))
+    }
+    return change(arr)
+ }
 
 module.exports = {
     decode
